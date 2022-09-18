60ceb4be417d70df994839577babc3aedd6996e0
GevManukyan <gevmanukyan110@gmail.com>
2022-09-18 19:29:20 +0300

    project first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+
+let str = document.createElement("p");
+document.body.appendChild(str)
+function func(){
+    str.classList.add("text")
+    str.innerText = `Welcome`
+   console.log(str);
+   
+}
+func()
+
